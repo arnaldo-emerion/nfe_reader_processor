e49e008d8c50971c5ee962bb7ad7616ec6d61349
Arnaldo Cicero Bezerra <arnaldotecadm@hotmail.com>
2022-02-20 23:56:07 +0000

    Fix

--- src/main/java/br/com/arcasoftware/sbs/listener/OrderMessageListener.java
+++ src/main/java/br/com/arcasoftware/sbs/listener/OrderMessageListener.java
@@ -3,52 +3,49 @@ package br.com.arcasoftware.sbs.listener;
 import br.com.arcasoftware.sbs.service.NfeProcessor;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.*;
-import br.com.arcasoftware.sbs.service.OrderService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class OrderMessageListener {
 
-    public static final String BUCKET_NAME = "nfe-reader-xml";
-    private static final ObjectMapper OBJECT_MAPPER = Jackson2ObjectMapperBuilder.json().build();
-    private final OrderService orderService;
+    private static final String BUCKET_NAME = "nfereader232856-dev";
+    private static final String REGION = "us-east-1";
+    private static final String QUEUE_NAME = "https://sqs.us-east-1.amazonaws.com/492510987777/nfeReaderQueue";
     private final NfeProcessor nfeProcessor;
 
-    public OrderMessageListener(OrderService orderService, NfeProcessor nfeProcessor) {
-        this.orderService = orderService;
+    public OrderMessageListener(NfeProcessor nfeProcessor) {
         this.nfeProcessor = nfeProcessor;
     }
 
-    @SqsListener(value = "https://sqs.us-east-1.amazonaws.com/492510987777/FileUploadQueue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    @SqsListener(value = QUEUE_NAME, deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void processMessage(Object message) {
         try {
             log.info("Received new SQS message: {}", message);
 
-            final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion("us-east-1").build();
+            final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(REGION).build();
 
+            String s3FileName = java.net.URLDecoder.decode(message.toString(), StandardCharsets.UTF_8.name());
 
-            S3Object o = s3.getObject(BUCKET_NAME, message.toString());
+            S3Object o = s3.getObject(BUCKET_NAME, s3FileName);
+
             S3ObjectInputStream s3is = o.getObjectContent();
 
-            this.nfeProcessor.processNFe(s3is, message.toString().split("/")[0], true);
+            String originalFileName = message.toString().split("/")[2];
+
+            this.nfeProcessor.processNFe(s3is, originalFileName, true);
 
             log.info("Processamento Finalizado");
 
         } catch (Exception e) {
-            throw new RuntimeException("Cannot process message from SQS", e);
+            log.error("It was not possible to process this NFe because: " + e.getMessage());
         }
     }
 }
--- src/main/java/br/com/arcasoftware/sbs/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package br.com.arcasoftware.sbs.service;
-
-import br.com.arcasoftware.sbs.dto.OrderDto;
-
-public interface OrderService {
-    void processOrder(OrderDto orderDto);
-    OrderDto placeOrder(OrderDto orderDto);
-}
